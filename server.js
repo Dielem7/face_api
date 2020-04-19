762e2d36db91bec04c986b6af1a67213502f1851
Ruan Lemmer <Lemmer@Ruans-MacBook-Air.local>
2020-04-19 09:24:24 +0200

    adding database Connection

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host: 'postgresql-crystalline-70692',
-    user: 'postgres',
-    password: 'Lemmer07',
-    database: 'smart-brain'
+    host: process.env.DATABASE_URL,
+   ssl: true,
   }
 });
 
